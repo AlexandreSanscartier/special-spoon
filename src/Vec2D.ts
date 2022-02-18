c59cc7ed300f5b76f1d4962525c2f04e719e9559
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2022-02-18 15:07:03 -0500

    Added scaleInPlace and calculating magnitude. Also added mechanism for only recalulating the magnitude if required.

--- src/Vec2D.ts
+++ src/Vec2D.ts
@@ -1,6 +1,9 @@
 export default class Vec2D {
 
     private _vector2D: Float32Array;
+    private _magnitude : number;
+
+    private _mustRecalculateMagnitude : boolean = true;
 
     public constructor(x : number, y : number) {
         this._vector2D = new Float32Array(2);
@@ -13,7 +16,11 @@ export default class Vec2D {
     }
 
     public set x(value) {
+        const isSame = value === this.x;
         this._vector2D[0] = value;
+        if(!isSame) {
+            this._mustRecalculateMagnitude = true;
+        }
     }
 
     public get x() {
@@ -21,11 +28,33 @@ export default class Vec2D {
     }
 
     public set y(value) {
+        const isSame = value === this.y;
         this._vector2D[1] = value;
+        if(!isSame) {
+            this._mustRecalculateMagnitude = true;
+        }
     }
 
     public get y() {
         return this._vector2D[1];
+    }
+
+    public scale(s : number) : Vec2D {
+        return new Vec2D(this.x * s, this.y * 2);
+    }
+
+    public scaleInPlace(s: number) {
+        this.x = this.x * s;
+        this.y = this.y * s;
+        this._mustRecalculateMagnitude = true;
+    }
+
+    public magnitude() : number {
+        if(this._mustRecalculateMagnitude) {
+            this._magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
+            this._mustRecalculateMagnitude = false;
+        }
+        return this._magnitude;
     }
 
     public toString() : string {
--- tests/Vec2D.test.ts
+++ tests/Vec2D.test.ts
@@ -1,9 +1,19 @@
 import Vec2D from '../src/Vec2D';
 
 describe('Vec2D class', () => {
+
+    let testVec2D : Vec2D;
+
+    beforeEach(() => {
+        testVec2D = new Vec2D(10, 22);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+      });
+
     it("should return the 10 as the value for x of the Vec2D(10, 22)", () => {
         // Arrange
-        var testVec2D = new Vec2D(10, 22);
         var expected = 10;
         var actual = testVec2D.x;
 
@@ -13,7 +23,6 @@ describe('Vec2D class', () => {
 
     it("should return the 22 as the value for y of the Vec2D(10, 22)", () => {
         // Arrange
-        var testVec2D = new Vec2D(10, 22);
         var expected = 22;
         var actual = testVec2D.y;
 
@@ -21,9 +30,8 @@ describe('Vec2D class', () => {
         expect(actual).toBe(expected);
     });
 
-    it("should return vec2(10, 22) as the value for toString() of the Vec2D(10, 22)", () => {
+    it("should return Vec2(10, 22) as the value for toString() of the Vec2D(10, 22)", () => {
         // Arrange
-        var testVec2D = new Vec2D(10, 22);
         var expected = 'vec2(10, 22)';
         var actual = testVec2D.toString();
 
@@ -31,9 +39,8 @@ describe('Vec2D class', () => {
         expect(actual).toBe(expected);
     });
 
-    it("should return a new vec2 with x: 10, y:22 as the value for Vec2D(10, 22).clone()", () => {
+    it("should return a new Vec2D with x: 10, y:22 as the value for Vec2D(10, 22).clone()", () => {
         // Arrange
-        var testVec2D = new Vec2D(10, 22);
         var expected = new Vec2D(10, 22);
 
         // Act
@@ -43,9 +50,121 @@ describe('Vec2D class', () => {
         expect(actual).toStrictEqual(expected);
     });
 
+    it("should return a new Vec2D with x:20 and y:44 scaled by a factor of 2 Vec2D(10, 22).scale(2)", () => {
+        // Arrange
+        var expected = new Vec2D(20, 44);
+        var expectedOriginal = new Vec2D(10, 22);
+
+        // Act
+        var actual = testVec2D.scale(2);
+
+        // Assert
+        expect(actual).toStrictEqual(expected);
+        expect(testVec2D).toStrictEqual(expectedOriginal);
+    });
+
+    it("should scale in place the Vec2D with x:20 and y:44 scaled by a factor of 2 Vec2D(10, 22).scaleInPlace(2)", () => {
+        // Arrange
+        var expectedX = 20;
+        var expectedY = 44;
+
+        // Act
+        testVec2D.scaleInPlace(2);
+
+        // Assert
+        expect(testVec2D.x).toBe(expectedX);
+        expect(testVec2D.y).toBe(expectedY);
+    });
+
+    it("should return the magnitude of Vec2D with x:10 and y:22 of 24.1660919472 Vec2D(10, 22).magnitude()", () => {
+        // Arrange
+        var expected = 24.166091947189145;
+
+        // Act
+        var actual = testVec2D.magnitude();
+
+        // Assert
+        expect(actual).toBe(expected);
+    });
+
+    it("should calculate the magnitude if this is the first call to Vec2D(10, 22).magnitude()", () => {
+        // Arrange
+        const sqrtSpy = jest.spyOn(global.Math, 'sqrt');
+
+        // Act
+        testVec2D.magnitude();
+
+        // Assert
+        expect(sqrtSpy).toBeCalledTimes(1);
+    });
+
+    it("should calculate the magnitude only one the first call to Vec2D(10, 22).magnitude()", () => {
+        // Arrange
+        const sqrtSpy = jest.spyOn(global.Math, 'sqrt');
+
+        // Act
+        for(let i = 0; i < 10; i++) {
+            testVec2D.magnitude();
+        }
+
+        // Assert
+        expect(sqrtSpy).toBeCalledTimes(1);
+    });
+
+    it("should calculate the magnitude after a change in x", () => {
+        // Arrange
+        const sqrtSpy = jest.spyOn(global.Math, 'sqrt');
+
+        // Act
+        testVec2D.magnitude();
+        testVec2D.x = 11;
+        testVec2D.magnitude();
+
+        // Assert
+        expect(sqrtSpy).toBeCalledTimes(2);
+    });
+
+    it("should not recalculate the magnitude when the previous x is equal to the new x after a change in x", () => {
+        // Arrange
+        const sqrtSpy = jest.spyOn(global.Math, 'sqrt');
+
+        // Act
+        testVec2D.magnitude();
+        testVec2D.x = 10;
+        testVec2D.magnitude();
+
+        // Assert
+        expect(sqrtSpy).toBeCalledTimes(1);
+    });  
+
+    it("should calculate the magnitude after a change in y", () => {
+        // Arrange
+        const sqrtSpy = jest.spyOn(global.Math, 'sqrt');
+
+        // Act
+        testVec2D.magnitude();
+        testVec2D.y = 30;
+        testVec2D.magnitude();
+
+        // Assert
+        expect(sqrtSpy).toBeCalledTimes(2);
+    });
+
+    it("should not recalculate the magnitude when the previous y is equal to the new y after a change in y", () => {
+        // Arrange
+        const sqrtSpy = jest.spyOn(global.Math, 'sqrt');
+
+        // Act
+        testVec2D.magnitude();
+        testVec2D.y = 22;
+        testVec2D.magnitude();
+
+        // Assert
+        expect(sqrtSpy).toBeCalledTimes(1);
+    });
+
     it("should modify the x value of the original and not the clone", () => {
         // Arrange
-        var testVec2D = new Vec2D(10, 22);
         var expected = 5;
         var expectedCloneX = 10;
 
